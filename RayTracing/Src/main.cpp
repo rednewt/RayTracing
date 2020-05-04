6336ac66fccea6f1d166f2f0b80cf6d5d0caa2d0
Abhijeet Zala <abhijeetzala81@gmail.com>
2020-05-05 03:55:08 +0530

    Sphere normals

--- RayTracing/Src/main.cpp
+++ RayTracing/Src/main.cpp
@@ -1,22 +1,33 @@
 #include "ray.h"
 
-bool HitSphere(const Vec3& sphereCenter, const Ray& ray, double sphereRadius)
+double HitSphere(const Vec3& sphereCenter, const Ray& ray, double sphereRadius)
 {
     auto oc = ray.origin - sphereCenter;
     
+    //solves quadratic equation of ray-sphere intersection (simplified)
     auto a = ray.direction.LengthSquared();
-    auto b = 2.0 * VectorDot(ray.direction, oc);
+    auto half_b = VectorDot(ray.direction, oc);
     auto c = oc.LengthSquared() - sphereRadius * sphereRadius;
-    
-    return b*b - 4*a*c > 0;
+
+    auto D = half_b*half_b - a*c;
+
+    if (D < 0)
+        return -1.0;
+
+    return (-half_b - sqrt(D)) / a;
 }
 
 
 Vec3 RayColor(const Ray& ray)
 {
-    if (HitSphere(Vec3(0.0, 0.0, -1.0), ray, 0.5))
-        return Vec3(1.0, 0.0, 0.0);
+    auto t = HitSphere(Vec3(0.0, 0.0, -1.0), ray, 0.5);
 
+    if (t > 0.0)
+    {
+        Vec3 normal = VectorNormalize(ray.At(t) - Vec3(0.0, 0.0, -1.0));
+        return 0.5 * (normal + Vec3(1.0, 1.0, 1.0));
+    }
+        
     Vec3 normalized = VectorNormalize(ray.direction);
     double y = 0.5 * (normalized.y + 1.0); //convert to [0,1] from [-1, 1]
     
@@ -29,8 +40,8 @@ Vec3 RayColor(const Ray& ray)
 
 int main() 
 {
-    const int canvasWidth = 200;
-    const int canvasHeight = 100;
+    const int canvasWidth = 800;
+    const int canvasHeight = 400;
 
     std::cout << "P3\n" << canvasWidth << ' ' << canvasHeight << "\n255\n";
 
